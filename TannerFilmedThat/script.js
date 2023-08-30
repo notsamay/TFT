a08cbd61b91949d67b2a2cc474bab25ce91552ee
notsamay <78178628+notsamay@users.noreply.github.com>
2023-08-29 20:40:01 -0400

    Add files via upload

--- TannerFilmedThat/script.js
+++ TannerFilmedThat/script.js
@@ -1,16 +1,16 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.1) {
         entry.target.classList.add('show');
       } else {
         entry.target.classList.remove('show');
       }
     });
-  }, { threshold: 0.2 });
+  }, { threshold: 0.5 });
   
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el, index) => {
-    const delay = 200 * index; // Adjust the delay value as needed (milliseconds)
+    const delay = 0 * index; // Adjust the delay value as needed (milliseconds)
     setTimeout(() => observer.observe(el), delay);
   });
   window.addEventListener('load', () => {
